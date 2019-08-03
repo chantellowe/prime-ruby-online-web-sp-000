9857c3e37b3319cce2137127d10eff4498b58a28
Chantel Lowe <chantellowe17@gmail.com>
2019-08-03 20:23:55 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -7,7 +7,8 @@ start = 2 #2 is the first prime number
 if num > 1 #1 by def is not a prime number 
   (start..number-1).to_a #convert range to array
   range.none? do |test|
-  
+    num % test == 0 
+    
 
 
 end
