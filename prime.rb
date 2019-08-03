001e7943a625288d651a36a60fb4cc7fafd7a440
Chantel Lowe <chantellowe17@gmail.com>
2019-08-03 20:53:45 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -3,13 +3,6 @@
 
 def prime?(number)
 
-  start = 2 #2 is the first prime number
-  if number > 1 #1 by def is not a prime number 
-    (start..number-1).to_a #convert range to array
-    range.none? do |test|
-      number % test == 0 
-    end
-  else
-    false
-  end
+  
+  
 end
