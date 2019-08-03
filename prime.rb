53d35de3e62710f673bb5c54b447fd15cd34202a
Chantel Lowe <chantellowe17@gmail.com>
2019-08-03 20:06:59 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,22 +1,8 @@
 # Add  code here!
 
 
-def prime?(i)
-  i > 1
-  
-  if (i / 2) && i != 2 
-    false
-  elsif (i / 3) && i != 3
-    false
-  elsif (i / 5) && i != 5
-    false
-  elsif (i / 7) && i != 7
-    false
-  else
-    true
-  end
-  
-  if i == 2
-    true
-  end
+def prime?(num)
+
+
+
 end
