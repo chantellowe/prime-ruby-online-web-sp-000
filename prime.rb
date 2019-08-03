fa0dcab615736f445b1651cf4108c0bed980dfe8
Chantel Lowe <chantellowe17@gmail.com>
2019-08-03 20:52:57 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,13 +1,13 @@
 # Add  code here!
 
 
-def prime?(num)
+def prime?(number)
 
   start = 2 #2 is the first prime number
-  if num > 1 #1 by def is not a prime number 
-    (start..num-1).to_a #convert range to array
+  if number > 1 #1 by def is not a prime number 
+    (start..number-1).to_a #convert range to array
     range.none? do |test|
-      num % test == 0 
+      number % test == 0 
     end
   else
     false
